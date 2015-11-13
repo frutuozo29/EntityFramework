0a4a4fc8060ff3930df4f99cfe5b6e481f43d9ae
renanfrutuozo <frutuozo29@gmail.com>
2015-11-13 18:57:32 -0300

    Conexão com banco SQL Server

--- ConsoleApp/ConsoleAppEntityFramework/ConsoleAppEntityFramework/Program.cs
+++ ConsoleApp/ConsoleAppEntityFramework/ConsoleAppEntityFramework/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ConsoleAppEntityFramework
 {
@@ -10,6 +6,57 @@ namespace ConsoleAppEntityFramework
     {
         static void Main(string[] args)
         {
+            using (var ctx = new AppContext())
+            {
+                var aluno1 = new Aluno
+                {
+                    Nome = "Renan Frutuozo",
+                    Endereco = new Endereco
+                    {
+                        Logradouro = "Avenida XXVI",
+                        CEP = "61800000",
+                        Numero = 402
+                    }
+                };
+
+                var aluno2 = new Aluno
+                {
+                    Nome = "Rodolffo Frutuozo",
+                    Endereco = new Endereco
+                    {
+                        Logradouro = "Teresina",
+                        CEP = "42500690",
+                        Numero = 2526
+                    }
+                };
+
+
+                var professor = new Professor
+                {
+                    Nome = "Ribeiro Batista",
+                    Endereco = new Endereco
+                    {
+                        Logradouro = "Rua da Pindaiba",
+                        CEP = "69500000",
+                        Numero = 700
+                    }
+                };
+
+                var turma = new Turma
+                {
+                    Vagas = 2,
+                    Alunos = new List<Aluno>()
+                };
+
+
+                turma.Professor = professor;
+                turma.Alunos.Add(aluno1);
+                turma.Alunos.Add(aluno2);
+
+                ctx.Turmas.Add(turma);
+                ctx.SaveChanges();
+            }
+
         }
     }
 }
