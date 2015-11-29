7fc3932fa69091d8e88a923694fd5e91332e4ca6
renanfrutuozo <frutuozo29@gmail.com>
2015-11-29 14:26:11 -0300

    Alterações

---
+++ AptNetMVC/CamadaDeControle/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using CamadaDeControle.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CamadaDeControle.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Listar(double? precoMinimo, double? precoMaximo)
+        {
+            var ctx = new AppContext();
+            var produtos = ctx.Produtos.AsEnumerable();
+
+            if (precoMinimo != null && precoMaximo != null)
+            {
+                produtos = from p in produtos
+                           where p.Preco >= precoMinimo & p.Preco <= precoMaximo
+                           select p;
+            }
+            return View(produtos);
+        }
+
+        [HttpGet]
+        public ActionResult Cadastra()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastra(Produto p)
+        {
+            var ctx = new AppContext();
+            ctx.Produtos.Add(p);
+            ctx.SaveChanges();
+
+            TempData["Mensagem"] = "Produto cadastrado com Sucesso";
+            return RedirectToAction("Listar");
+        }
+    }
+}
---
+++ AptNetMVC/CamadaDeControle/Models/AppContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace CamadaDeControle.Models
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
