c047baea373f69ad8f8f7f99b0affbe367b6548c
renanfrutuozo <frutuozo29@gmail.com>
2015-12-05 13:54:30 -0300

    Comitando o projeto de Filtros e Tratamento de Erros

---
+++ AptNetMVC/Filtros/Filters/LoggingFilter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filtros.Filters
+{
+    public class LoggingFilter : IActionFilter
+    {
+        private static string fileName = "Requisicoesinfo.log";
+
+        private static string logFilePath;
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void SaveInfo(ControllerContext context)
+        {
+            string info = String.Format("Usuário:{0}, IP:{1}, Data/Hora:{2}, URL:{3}",
+                context.HttpContext.User.Identity.Name,
+                context.HttpContext.Request.UserHostAddress,
+                DateTime.Now,
+                context.HttpContext.Request.RawUrl);
+            var path = GetLogFilePath(context);
+            using (var logWrite = new StreamWriter(path, true))
+            {
+                logWrite.WriteLine(info);
+            }
+        }
+
+        private static string GetLogFilePath(ControllerContext context)
+        {
+            if (LoggingFilter.logFilePath == null)
+            {
+                var logPath = context.HttpContext.Server.MapPath("~/Logs");
+                if (!Directory.Exists(logPath))
+                {
+                    Directory.CreateDirectory(logPath);
+                }
+                var path = Path.Combine(logPath, fileName);
+                LoggingFilter.logFilePath = path;
+            }
+            return LoggingFilter.logFilePath;
+        }
+    }
+}
---
+++ AptNetMVC/TratamentoDeErros/Controllers/TesteController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace TratamentoDeErros.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult TestaErro()
+        {
+            string[] nomes = new string[] { "Renan", "Rodolffo" };
+            string nome = nomes[2];
+            return View();
+        }
+    }
+}
